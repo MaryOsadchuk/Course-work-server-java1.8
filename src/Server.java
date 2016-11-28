ce1e6480beeea4772540b46f3a2eef42837a7a9e
Tsvihunenko <vladimirtsvigunenko@gmail.com>
2016-11-28 22:40:17 +0400

    Add server socket;
    Takoe se6e

--- src/Server.java
+++ src/Server.java
@@ -1,19 +1,72 @@
 import data.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
 
 /**
  * Created by Vladimir on 27.11.2016.
  */
 public class Server {
-   private int id = 0;
-   private String  user="";
-   private String  password="";
+//   private int id = 0;
+  // private String  user="";
+  // private String  password="";
+
+public static final  int PORT = 29757;
+
+ private ServerSocket serversocket = null;
+
+
+    public void setSocketConnection() {
+        Socket socket=new Socket();
+        try {
+            serversocket = new ServerSocket(PORT);
+            System.out.println("Server connection was fully created");
+
+            socket = serversocket.accept();
+        }catch(IOException e){
+
+        }
+        while (true) {
+
+            try {
+
+
+
+                System.out.println("Connection was fully accepted");
+                try (
+                        InputStream in = socket.getInputStream();
+                        OutputStream out = socket.getOutputStream();) {
+                    byte[] buf = new byte[32 * 1024];
+                    int readBytes = in.read(buf);
+                    String exitmessage = new String(buf, 0, readBytes);
+                    System.out.println("Client>" + exitmessage);
+
+                    out.write(exitmessage.getBytes());
+                    out.flush();
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("Not real");
+            }
+
+        }
+    }
+
+
+
+
 
 
 
     public static void main(String[] args) {
-       SQLiteJDBC r = new SQLiteJDBC();
+        /*   SQLiteJDBC r = new SQLiteJDBC();
         Scanner in = new Scanner(System.in);
 
         //int user =in.nextInt();
@@ -30,7 +83,18 @@ public class Server {
         //password=r.getPassword(id);
         //r.deleteUser(0);
        // System.out.println(id);
-     //       r.printAllList();
+
+
+
+
+
+        //       r.printAllList();*/
+
+        Server a = new Server();
+
+        a.setSocketConnection();
+
+
     }
 
 }
